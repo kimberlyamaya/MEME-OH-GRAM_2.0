6bc2db2a47795802104643a53aab287627875ee2
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-03-13 21:53:16 -0700

    updated client files

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,32 @@
- import logo from './logo.svg';
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // import {setContext} from "@apollo/client/link/context"
-import './App.css';
-import Navbar from "./components/Navbar/Navbar"
-import Home from './components/Home/Home';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Memes from "./components/Memes/Memes";
-import LoginForm from "./components/LoginForm/LoginForm";
-import SignupForm from "./components/SignupForm/Signup";
+// import LoginForm from "./components/LoginForm/LoginForm";
+// import SignupForm from "./components/SignupForm/Signup";
 
 function App() {
   return (
     // <ApolloProvider>
     <Router>
-    <Navbar />
-    <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/home" element={<Home/>}/>
-    <Route path="/about" element={<About/>}/>
-    <Route path="/memes" element={<Memes/>}/>
-    <Route path="/loginForm" element={<LoginForm/>}/>
-    <Route path="/signupForm" element={<SignupForm/>}/>
-    <Route path="/logout" element={<LoginForm/>}/>
-     </Routes>
-</Router>);
-
-
-   
-
-   )
-  };
-
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/memes" element={<Memes />} />
+        <Route path="/loginForm" element={<LoginForm />} />
+        {/* <Route path="/signupForm" element={<SignupForm />} />
+        <Route path="/logout" element={<LoginForm />} /> */}
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
