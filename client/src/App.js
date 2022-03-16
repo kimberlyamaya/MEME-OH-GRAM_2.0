c7f03a828ceceff611b7e04c6dbe4d49b72e1796
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-03-15 21:57:10 -0700

    syncced files wioth test branch

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import Memes from "./components/Meme/Meme";
 import LoginForm from "./components/LoginForm/LoginForm";
 import SignupForm from "./components/SignupForm/Signup";
 import Profile from "./components/Profile/Profile";
+import MemeGenerator from "./components/Meme-Generator";
 
 // -ka added 3/14
 import {ApolloClient,
@@ -48,81 +49,21 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/memes" element={<Memes/>}/>
-        <Route path="/loginForm" element={<LoginForm/>}/>
-        <Route path="/signupForm" element={<SignupForm/>}/>
-        <Route path="/logout" element={<LoginForm/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-      </Routes>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/memes" element={<Memes />} />
+          <Route path="/loginForm" element={<LoginForm />} />
+          <Route path="/signupForm" element={<SignupForm />} />
+          <Route path="/logout" element={<LoginForm />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/meme" element={<MemeGenerator />} />
+        </Routes>
       </Router>
     </ApolloProvider>
   );
 }
 
 export default App;
-
-// *************** SETHS ***************
-
-// import React, { useState } from 'react';
-// import './App.css';
-// import Home from './Home'
-// import Profile from './Profile'
-// import NewMeme from './NewMeme'
-// import Meme from './Meme';
-// import Navbar from '../src/components/Navbar/Navbar'
-
-// function App() {
-
-//   const [isRed, setRed] = useState(false);
-//   const [isPurple, setPurple] = useState(false);
-
-//   const [count, setCount] = useState(0);
-
-//   const increment = () => {
-//     setCount(count + 1)
-//     setRed(!isRed)
-
-//     // const increment = () => {
-//     //   setCount(count + 1)
-//     //   setPurple(!isPurple)
-
-//   }
-//   return (
-
-//     <div className="App">
-
-//       <div>
-//         <Home />
-//         <h3 className={isRed ? 'memeButtons' : ""}>Likez</h3>
-//         <button onClick={increment}>Liker</button>
-//         <h5>{count}</h5>
-
-//         <Meme name="Meme1" likes={count} />
-//         <Meme name="Meme2" likes={count} />
-//         <Meme name="Meme3" likes={count} />
-//         <Meme name="Meme4" likes={count} />
-
-//       </div>
-
-//       <Profile />
-
-//       <NewMeme name="new1" likes="" />
-//       <NewMeme name="new2" likes="" />
-//       <NewMeme name="new3" likes="" />
-//       <NewMeme name="new4" likes="" />
-
-//     </div>
-
-//   );
-// }
-
-// export default App;
-
-// <>
-
-// </>
